892a6fb4cdbe6bebe502c6ed526c4ec22a61ce74
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-10-04 00:21:50 +0100

    add vidio 3 and start 4

---
+++ learn_to_program/4-String_Functions.py
@@ -0,0 +1,12 @@
+string = "    hello from nadjib   "
+string = string.lstrip()
+string = string.rstrip()
+string = string.strip()
+print(string.capitalize())
+print((string.upper()))
+print(string.lower())
+a_list = ["Bunch", "of", "random", "words"]
+print(" ".join(a_list))
+a_list_2 = string.split()
+for i in a_list_2:
+    print(i)
