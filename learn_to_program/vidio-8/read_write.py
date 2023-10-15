390703e55cb233ec7032480e01e73382f94d3501
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-10-15 20:00:02 +0100

    update 08-10-2023

--- learn_to_program/vidio-8/read_write.py
+++ learn_to_program/vidio-8/read_write.py
@@ -3,5 +3,15 @@ import os
 with open("test.txt", mode="w", encoding="utf-8") as myFile:
     myFile.write("Hello World\nI'am Nadjib and iam a software enginner")
 
-with open("test.txt", encoding="utf-8") as MyFile:
-    print(MyFile.read())
+with open("test.txt", encoding="utf-8") as myFile:
+    print(myFile.read())
+
+# os.rename("test.txt", "myData.txt")
+os.remove("myData.txt")
+
+"""
+print(myFile.closed)
+print(myFile.name)
+print(myFile.mode)
+"""
+
