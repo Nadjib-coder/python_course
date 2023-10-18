6e151df0e566ea944230eb9349503e6013b22b3b
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-10-18 15:45:17 +0100

    update 08-10-2023

---
+++ learn_to_program/vidio-9/LearnTest.py
@@ -0,0 +1,36 @@
+import unittest
+
+
+def _sum(a, b):
+    return a + b
+
+
+class LearnTest(unittest.TestCase):
+    def setUp(self):
+        print('SETUP Called ...')
+        # Arrange
+        self.a = 10
+        self.b = 20
+
+    def tearDown(self):
+        self.a = 0
+        self.a = 0
+        print('tearDown Called ...')
+
+    def test_sum_func_1(self):
+        print('TEST - 1 Called ...')
+        # Act
+        result = _sum(self.a, self.b)
+        # Assert
+        self.assertEqual(result, self.a + self.b)
+
+    def test_sum_func_2(self):
+        print('TEST - 2 Called ...')
+        # Act
+        result = _sum(self.b, self.a)
+        # Assert
+        self.assertEqual(result, self.a + self.b)
+
+
+if __name__ == "__main__":
+    unittest.main()
