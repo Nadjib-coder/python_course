57489d1d21dc9bc6aa8ce527a51d064e1365ff30
Nadjib-coder <nadjibmouhoun1@gmail.com>
2023-11-11 23:11:20 +0100

    uodate 17/10/2023

--- Test_course/LearnTest-2.py
+++ Test_course/LearnTest-2.py
@@ -6,9 +6,8 @@ class LearnTest(unittest.TestCase):
         a = [1, 2, 3]
         b = [1, 3, 3]
         c = a
-        #self.assertEqual(a, b, msg="1 is not equal to 10 haha")
+        # self.assertEqual(a, b, msg="1 is not equal to 10 haha")
         self.assertIs(a, c)
-
 
 
 if __name__ == "__main__":
