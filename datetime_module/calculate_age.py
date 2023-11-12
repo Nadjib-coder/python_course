1f4c9cab18dc2ddf1f78703956a5d75bf7dbf3fd
Nadjib-coder <nadjibmouhoun1@gmail.com>
2023-11-12 18:41:16 +0100

    uodate 17/10/2023

--- datetime_module/calculate_age.py
+++ datetime_module/calculate_age.py
@@ -4,8 +4,8 @@ from datetime import datetime
 def calculate_age(birthdate):
     today = datetime.today()
     birthdate = datetime.strptime(birthdate, "%Y-%m-%d")
-    age = today.year - birthdate.year
-    # - ((today.month, today.day) < (birthdate.month, birthdate.day)))
+    age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
     print("You Age:", age)
+
 
 calculate_age("2002-03-28")
