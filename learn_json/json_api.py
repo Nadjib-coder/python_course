fee7bc1fb409dbe88555688318862a7831595a31
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-11-12 10:18:32 +0100

    19-10-2023

--- learn_json/json_api.py
+++ learn_json/json_api.py
@@ -1,7 +1,21 @@
 import json
 from urllib.request import urlopen
 
-with urlopen("https://query.yahooapis.com") as response:
-    source = response.read()
+with urlopen("https://query.yahooapis.com") as file:
+    str_content = file.read()
 
-print(source)
+data = json.loads(str_content)
+
+print(json.dumps(data, indent=2))
+
+print(len(data['list']['resources']))
+
+usd_rates = dict()
+
+for item in data['list']['resources']:
+    name = item['resource']['field']['name']
+    price = item['resource']['field']['price']
+    # print(name, price)
+    usd_rates[name] = price
+
+print(50 * float(usd_rates['USD/EUR']))
