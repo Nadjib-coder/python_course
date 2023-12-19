040bf15569fc6b9a571045630ac3e7c5fa4940d6
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-12-19 19:31:29 +0100

    sqlalchemy

--- MySQL/connect_to_MySQL_sqlalchemy.py
+++ MySQL/connect_to_MySQL_sqlalchemy.py
@@ -1,1 +1,23 @@
 from sqlalchemy import create_engine, Metadata, Tabel
+
+host = "localhost"
+user = "user_name"
+password = "user_password"
+database = "db_name"
+
+engine = create_engine(f"mysql://{user}:{password}@{host}/{database}", echo=True)
+
+connection = engine.connect()
+
+metadata = Metadata()
+
+our_tabel = Tbale('table_name', metadata, autoload_with=engine)
+
+query = our_tabel.select()
+
+result = connection.excute(query)
+
+for row in result:
+    print(row)
+
+connection.close()
