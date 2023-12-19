3e9060bf3b84dfbec71c1a0e7aec741497c41530
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-12-19 17:34:59 +0100

    binary tree

---
+++ MySQL/connetc_to_MySQL.py
@@ -0,0 +1,22 @@
+import MySQLdb
+
+host = "localhost"
+user = "user_name"
+passwd = "user_password"
+db = "db_name"
+
+connection = MySQLdb.connect(host=host, user=user, passwod=passwod, db=db)
+
+cur = connection.cursor()
+
+query = "SELECT * FROM table_name"
+
+cur.execute(query)
+
+result = cur.fetchall()
+
+for row in result:
+    print(row)
+
+cur.close()
+connection.close()
